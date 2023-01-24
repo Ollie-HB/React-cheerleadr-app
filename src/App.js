64ef5bef72dd9427a4aeb0ca396b2eed234e20bb
Ollie Beck <ollieb94@hotmail.co.uk>
2023-01-24 11:15:23 +0000

    handleClick function added for buttons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 const App = () => {
   const word = 'LIVERPOOL';
 
+  const handleClick = () => {
+    console.log("Clicked!")
+  }
+
   return (
     <div className="App">
       <h1>LIVERPOOL</h1>
     <div>
       {word.split("").map((char) => {
-        return <button>{char}</button>
+        return <button onClick={handleClick}>{char}</button>
       })}
     </div>
      {word}

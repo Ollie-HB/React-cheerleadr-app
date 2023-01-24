ae3f4523087d6dac297807c14264da71ff1f3956
Ollie Beck <ollieb94@hotmail.co.uk>
2023-01-24 11:02:52 +0000

    Hardcoded buttons - need to make DRY

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ const App = () => {
   const word = 'LIVERPOOL';
 
   return (
-    <div className="App">{word}</div>
+    <div className="App">
+      <h1>{word}</h1>
+    <div>
+      <button>L</button>
+      <button>I</button>
+      <button>V</button>
+      <button>E</button>
+      <button>R</button>
+      <button>P</button>
+      <button>O</button>
+      <button>O</button>
+      <button>L</button>
+    </div>
+     {word}
+    </div>
   );
 }
 

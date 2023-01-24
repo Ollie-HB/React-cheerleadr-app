8d0e8f0dbda57210a5378a10adaf8c6ed1c67ac9
Ollie Beck <ollieb94@hotmail.co.uk>
2023-01-24 11:10:11 +0000

    Buttons now generated through map array

--- src/App.js
+++ src/App.js
@@ -5,17 +5,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>{word}</h1>
+      <h1>LIVERPOOL</h1>
     <div>
-      <button>L</button>
-      <button>I</button>
-      <button>V</button>
-      <button>E</button>
-      <button>R</button>
-      <button>P</button>
-      <button>O</button>
-      <button>O</button>
-      <button>L</button>
+      {word.split("").map((char) => {
+        return <button>{char}</button>
+      })}
     </div>
      {word}
     </div>
